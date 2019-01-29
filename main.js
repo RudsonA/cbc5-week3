f0123574e790cb7e3783d888a60f58a1241252bc
RudsonA <rudson.augustin@yahoo.com>
2019-01-29 15:24:01 -0500

    USING IF ELSE STATEMENT FOR THE GAME

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 "use strict"
 
+let missed = "You missed the bird";
+
+let hit = "You hit the bird";
+
+let game = prompt ("Do you want to shoot the birds?")
+
+if (game == "yes") {
+    alert(hit);
+    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+} else {
+    alert(missed);
+    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+}
+
+
