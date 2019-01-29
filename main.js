5d14820866541141a1ef013067d3e54489719fba
RudsonA <rudson.augustin@yahoo.com>
2019-01-29 16:03:49 -0500

    adding if and else if, removed string, soon to add number generator

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ let missed = "You missed the bird";
 
 let hit = "You hit the bird";
 
-let game = prompt ("Do you want to shoot the birds?")
+let picknumber = prompt ("Pick a number between 0 and 10")
 
-if (game == "yes") {
+if (picknumber == "0") {
     alert(hit);
     document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else {
+} else if (picknumber=="1") {
     alert(missed);
     document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-}
+} else if (picknumber=="2") {
+    alert(hit);
+    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+} else if (picknumber=="3") {
+    alert(missed);
+    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+} else if (picknumber=="4") {
+    alert(hit);
+} 
 
 
