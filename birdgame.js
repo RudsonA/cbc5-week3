b82c8d04d6917aa004e9e5b33d7f2fed8928dfd2
RudsonA <rudson.augustin@yahoo.com>
2019-01-30 16:08:13 -0500

    almost done with text based game with for loop

--- birdgame.js
+++ birdgame.js
@@ -1,30 +1,41 @@
 "use strict"
 
-let number = prompt ("Pick a number from zero to three")
-
 let missed = "You missed the bird!";
 
 let hit = "You hit the bird";
 
-let zeroToThree = "Pick a number from zero to three"
+let zeroToNine = "Pick a number from zero to nine"
 
 let onlyNumbers = "Only numbers please!"
 
 let grid = ("_|_|_\n _|_|_\n _|_|_\n")
 console.log(grid)
 
-let birdsPath = Math.floor(Math.random()*10)
 
-let birdwas = " Bird was at " + birdsPath
+for (let i = 3; i > 0; i--) { 
+    let number = prompt ("Pick a number from zero to nine")
+    let birdsPath = Math.floor(Math.random()*10)
+    let birdwas = " Bird was at " + birdsPath
+    let attempts = i 
+    let remainding = "You have " + (attempts-1) + " attempts remainding!"
 
-for (let i = 1; i < 4; i++) { 
 if (number==birdsPath) {
     alert(hit);
+    document.getElementById("game").style.backgroundImage="url('img/up.gif'";
     break
 } else {
     alert(missed + birdwas + "!"); 
+    alert (remainding)
+    document.getElementById("game").style.backgroundImage="url('img/down.gif'";
 } 
 }
+
+
+display()
+function display() {
+    document.getElementById("game").style.backgroundRepeat="no-repeat";
+}
+
 
 // math.floor(math.random()*4)
 // (firstInput === secondInput) 
--- main.js
+++ main.js
@@ -8,11 +8,11 @@ let oneToTen = "Pick a number one to ten"
 
 let onlyNumbers = "Only numbers please!"
 
-let picknumber = prompt ("Pick a number between 0 and 10")
+// let picknumber = prompt ("Pick a number between 0 and 10")
 
-let even = (picknumber % 2 !==0)
+// let even = (picknumber % 2 !==0)
 
-let odd = (picknumber % 2 == 0)
+// let odd = (picknumber % 2 == 0)
 
 
 // for (; picknumber % 2 !== 0; ) {
@@ -27,46 +27,55 @@ let odd = (picknumber % 2 == 0)
 //     if (!odd) break
 // }
 
-if (picknumber == "0") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber=="1") {
-    alert(missed);
-    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-} else if (picknumber=="2") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber=="3") {
-    alert(missed);
-    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-} else if (picknumber=="4") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber == "5") {
-    alert(missed); 
-    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-} else if (picknumber == "6") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber == "7") {
-    alert (missed);
-    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-} else if (picknumber=="8") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber=="9") {
-    alert(missed);
-    document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
-} else if (picknumber=="10") {
-    alert(hit);
-    document.getElementById("game").style.backgroundImage="url('img/home.gif')";
-} else if (picknumber > 10) {
-    alert(oneToTen);
-    document.getElementById("game").style.backgroundImage="url('img/oneToTen.png')";
-} else { 
-    alert (onlyNumbers) 
-    document.getElementById("game").style.backgroundImage="url('img/dead.png')";
+for (let i = 1; i < 6; i++) {
+    let picknumber = prompt ("Pick a number between 0 and 10");
+
+    // let even = (picknumber % 2 !==0)
+
+    // let odd = (picknumber % 2 == 0)
+    if (picknumber == "0") {
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+        // alert(hit);
+    } else if (picknumber=="1") {
+        alert(missed);
+        document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+    } else if (picknumber=="2") {
+        alert(hit);
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+    } else if (picknumber=="3") {
+        alert(missed);
+        document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+    } else if (picknumber=="4") {
+        alert(hit);
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+    } else if (picknumber == "5") {
+        alert(missed); 
+        document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+    } else if (picknumber == "6") {
+        alert(hit);
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+    } else if (picknumber == "7") {
+        alert (missed);
+        document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+    } else if (picknumber=="8") {
+        alert(hit);
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+    } else if (picknumber=="9") {
+        alert(missed);
+        document.getElementById("game").style.backgroundImage="url('img/flyaway.gif')";
+    } else if (picknumber=="10") {
+        alert(hit);
+        document.getElementById("game").style.backgroundImage="url('img/home.gif')";
+    } else if (picknumber > 10) {
+        alert(oneToTen);
+        document.getElementById("game").style.backgroundImage="url('img/oneToTen.png')";
+    } else { 
+        alert (onlyNumbers) 
+        document.getElementById("game").style.backgroundImage="url('img/dead.png')";
+        break;
+    }
 }
+
 
 function display() {
     document.getElementById("game").style.backgroundRepeat="no-repeat";
